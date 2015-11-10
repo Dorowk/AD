18bbd981c894993f9149444fcea4bb53b981126f
Dorowk <pablo.castellano.95@gmail.com>
2015-11-10 09:12:05 +0100

    Añadido el refresh antes de solucionarlo en clase

---
+++ PArticulo/PArticulo/BorrarView.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace PArticulo
+{
+	public partial class BorrarView : Gtk.Window
+	{
+		public BorrarView () : 
+				base(Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
