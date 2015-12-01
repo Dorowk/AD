bc0422c2e6831a45347c29a84122231805d2bc54
Dorowk <pablo.castellano.95@gmail.com>
2015-12-01 10:40:21 +0100

    ArticuloPersister sin terminar

--- GTK_Articulo/PArticulo/ArticuloView.cs
+++ GTK_Articulo/PArticulo/ArticuloView.cs
@@ -7,16 +7,17 @@ using GTK_Serpis;
 namespace PArticulo
 {	
 	public delegate void SaveDelegate();
+
 	public partial class ArticuloView : Gtk.Window	
 	{	
+		private Articulo articulo = new Articulo();
 		private object id;
-		private string nombre="";
-		private decimal precio =0;
 		private object categoria = null;
 		private SaveDelegate save;
 
 		public ArticuloView ():base(Gtk.WindowType.Toplevel)
 		{
+			articulo.Nombre = "";
 			init ();
 			save = insert;
 	
@@ -24,68 +25,40 @@ namespace PArticulo
 		public ArticuloView(object id):base(Gtk.WindowType.Toplevel)
 		{
 			this.id = id;
-			load ();
+			articulo.Nombre = "";
 			init ();
 			save = update;
 		}
 
 		private void init(){
 			this.Build ();
-			entryNombre.Text = nombre;
+			entryNombre.Text = articulo.Nombre;
 			QueryResult queryResult = PersisterHelper.Get ("select * from categoria");
-			ComboBoxHelper.Fill (comboBoxCategoria, queryResult,categoria);
-			spinButtonPrecio.Value = Convert.ToDouble (precio);
+			ComboBoxHelper.Fill (comboBoxCategoria, queryResult,articulo.Categoria);
+			spinButtonPrecio.Value = Convert.ToDouble (articulo.Precio);
 			saveAction.Activated += delegate {	save();	};
-
-
 		}
-		private void update(){
-			IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand ();
-			dbCommand.CommandText = "update articulo set nombre=@nombre, categoria=@categoria,"+
-			"precio=@precio where id=@id";
+	
 
-			nombre = entryNombre.Text;
-			categoria = ComboBoxHelper.GetId (comboBoxCategoria);
-			precio = Convert.ToDecimal(spinButtonPrecio.Value);
 
-			DbCommandHelper.AddParameter (dbCommand, "nombre", nombre);
-			DbCommandHelper.AddParameter (dbCommand, "categoria", categoria);
-			DbCommandHelper.AddParameter (dbCommand, "precio", precio);
-			DbCommandHelper.AddParameter (dbCommand, "id", id);
-			dbCommand.ExecuteNonQuery ();
+		private void insert() {
+			articulo.Nombre = entryNombre.Text;
+			articulo.Categoria = ComboBoxHelper.GetId (comboBoxCategoria);
+			articulo.Precio = Convert.ToDecimal(spinButtonPrecio.Value);
+
+			ArticuloPersister.Insert (articulo);
+
 			Destroy ();
 		}
 
-		private void load(){
-			IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand ();
-			dbCommand.CommandText = "select * from articulo where id = @id";
-			DbCommandHelper.AddParameter (dbCommand, "id", id);
-			IDataReader datareader = dbCommand.ExecuteReader();
-			if (!datareader.Read ())
-				;
-			nombre = (string)datareader ["nombre"];
-			categoria = datareader ["categoria"];
-			if (categoria is DBNull)
-				categoria = null;
-			precio = (decimal)datareader ["precio"];
-			datareader.Close ();
+		private void update(){
+			articulo.Nombre = entryNombre.Text;
+			articulo.Categoria = ComboBoxHelper.GetId (comboBoxCategoria);
+			articulo.Precio = Convert.ToDecimal(spinButtonPrecio.Value);
+			articulo.Id = id;	
 
+			ArticuloPersister.Insert (articulo);
 
-		}
-
-		private void insert() {
-			IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand ();
-			dbCommand.CommandText = "insert into articulo (nombre, categoria, precio) " +
-				"values (@nombre, @categoria, @precio)";
-
-			nombre = entryNombre.Text;
-			categoria = ComboBoxHelper.GetId (comboBoxCategoria);
-			precio = Convert.ToDecimal(spinButtonPrecio.Value);
-
-			DbCommandHelper.AddParameter (dbCommand, "nombre", nombre);
-			DbCommandHelper.AddParameter (dbCommand, "categoria", categoria);
-			DbCommandHelper.AddParameter (dbCommand, "precio", precio);
-			dbCommand.ExecuteNonQuery ();
 			Destroy ();
 		}
 	}
